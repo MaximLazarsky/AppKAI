2ac543a12caa6accdba832c1327a20c845050e6d
MaximLazarsky <lazarskiy@gmail.com>
2024-08-19 17:00:28 +0200

    fixed vallet

--- src/theme/reset.ts
+++ src/theme/reset.ts
@@ -80,7 +80,6 @@ const ResetStyles = createGlobalStyle`
 
   /* Remove all the styles of the "User-Agent-Stylesheet", except for the 'display' property */
   *:where(:not(iframe, canvas, img, svg, video):not(svg *)) {
-    all: unset;
     display: revert;
   }
 
